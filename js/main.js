4bb37f375be452125a3b21ae6d3983295804fd6a
IL1DARIAN <sergey_nosov1989@rambler.ru>
2019-10-02 14:16:38 +0300

    Дополнительное задание по клонированию элементов

--- js/main.js
+++ js/main.js
@@ -27,8 +27,8 @@ var getComments = function () {
   for (var i = MIN_COMMENT; i <= getRandom(MIN_COMMENT, MAX_COMMENT); i++) {
     comments.push({
       avatar: 'img/avatar-' + getRandom(MIN_AUTHOR, MAX_AUTHOR) + '.svg',
-      massage: totalComments[getRandom(0, totalComments.length)],
-      name: names[getRandom(0, names.length)]
+      massage: totalComments[getRandom(0, totalComments.length-1)],
+      name: names[getRandom(0, names.length-1)]
     });
   }
 
@@ -60,9 +60,44 @@ var renderPhotos = function (photoCard) {
   return photoElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < getPhotos().length; i++) {
-  fragment.appendChild(renderPhotos(getPhotos()[i]));
-}
+var totalPhotos = getPhotos();
 
-picturesCard.appendChild(fragment);
+var loadingPhotos = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < totalPhotos.length; i++) {
+    fragment.appendChild(renderPhotos(totalPhotos[i]));
+  }
+
+  return picturesCard.appendChild(fragment);
+};
+
+loadingPhotos();
+
+var bigPictures = document.querySelector('.big-picture');
+
+bigPictures.classList.remove('hidden');
+bigPictures.querySelector('.big-picture__img img').src = totalPhotos[0].url;
+bigPictures.querySelector('.likes-count').textContent = totalPhotos[0].likes;
+bigPictures.querySelector('.comments-count').textContent = totalPhotos[0].comments.length;
+bigPictures.querySelector('.social__caption').textContent = totalPhotos[0].description;
+bigPictures.querySelector('.social__comment-count').classList.add('visually-hidden');
+bigPictures.querySelector('.comments-loader').classList.add('visually-hidden');
+
+var socialComments = bigPictures.querySelector('.social__comments');
+var itemComment = socialComments.querySelector('.social__comment');
+
+var renderComment = function () {
+  var commentElement = itemComment.cloneNode(true);
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < totalPhotos[0].comments.length; i++) {
+    commentElement.querySelector('.social__picture').src = totalPhotos[0].comments[i].avatar;
+    commentElement.querySelector('.social__picture').alt = totalPhotos[0].comments[i].name;
+    commentElement.querySelector('.social__text').textContent = totalPhotos[0].comments[i].massage;
+
+    fragment.appendChild(commentElement);
+  };
+
+  return socialComments.appendChild(fragment);
+};
+
+renderComment();
